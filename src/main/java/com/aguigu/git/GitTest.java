8cb9051b4d68285b70799c6f165740b29bd9b77a
Viper <Viper@aguigu.com>
2022-05-02 21:57:22 +0800

    first commit

---
+++ src/main/java/com/aguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.aguigu.git;
+
+/**
+ * @author O-o
+ * @date 2022/5/2 21:52
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
