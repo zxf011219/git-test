594fbf889ba68fba8d3f15a74c1cca2fe890ce02
Viper <Viper@aguigu.com>
2022-05-02 21:59:24 +0800

    second commit

--- src/main/java/com/aguigu/git/GitTest.java
+++ src/main/java/com/aguigu/git/GitTest.java
@@ -8,5 +8,6 @@ package com.aguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
